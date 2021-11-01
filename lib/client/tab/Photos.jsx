08fd4c0989f708394dbd6b4c1162c7f0903c7ecd
mrstaf <ben82160@gmail.com>
2021-11-01 03:53:25 +0100

    feat: display photos

--- lib/client/tab/Photos.jsx
+++ lib/client/tab/Photos.jsx
@@ -1,17 +1,7 @@
 // React Native
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import {
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  StyleSheet,
-  FlatList,
-  SafeAreaView,
-  RefreshControl,
-  Pressable,
-} from "react-native";
+import { Text, View, Image, StyleSheet, FlatList, SafeAreaView, RefreshControl, Pressable } from "react-native";
 import * as SecureStore from "expo-secure-store";
 
 // SVG
@@ -22,126 +12,120 @@ import tw from "twrnc";
 import { vw } from "react-native-expo-viewport-units";
 
 // Assets
-import { logout } from "./../assets/icons";
+import { add } from "./../assets/icons";
 
-// Effects
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
 
-const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd6-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd6-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-14551e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-14557e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e9d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e2972",
-    title: "Third Item",
-  },
-];
-
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-  <TouchableOpacity
-    onPress={onPress}
-    style={[
-      tw`p-3 m-3 rounded-xl`,
-      backgroundColor,
-      { width: vw(42), height: vw(42) },
-    ]}
-  >
-    <Text style={[styles.title, textColor]}>{item.title}</Text>
-  </TouchableOpacity>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  title: {
-    fontSize: 32,
-  },
-});
-
-export default function Photos({ navigation, logged, setLogged }) {
-  const [data, setData] = useState(DATA);
+export default function Photos({ navigation, route }) {
+  const [data, setData] = useState([]);
   const [selectedId, setSelectedId] = useState(null);
   const [refreshing, setRefreshing] = useState(false);
-  // useLogin(navigation);
-  const handleLogOut = () => {
-    setLogged(false);
-    SecureStore.deleteItemAsync('user_');
-    SecureStore.deleteItemAsync('pass_');
-    navigation.navigate("LandPage")
-  }
-  const onRefresh = () => {
+  const onEndReached = () => {
     setRefreshing(true);
-    setData([
-      ...data,
-      {
-        id: Date.now(),
-        title: `${data.length} Title`,
-      },
-    ]);
+    SecureStore.getItemAsync("user_")
+      .then((user) => {
+        let userParsed = JSON.parse(user);
+        let headersList = {
+          Accept: "*/*",
+          "Content-Type": "application/json",
+          "auth-token": userParsed.token,
+        };
+        fetch(`https://kloud.benoit.fage.fr/api/photos/all/?start=${data.length}&limit=${data.length + 2}`, {
+          method: "GET",
+          headers: headersList,
+        })
+          .then(function (response) {
+            return response.json();
+          })
+          .then((fetched_data) => {
+            // TODO: Alert when fetched_data.data.length = 0;
+            console.log("fetched_onEndReached", fetched_data.data);
+            const fetched_data_ = fetched_data.data.map((id) => {
+              return {
+                id: id,
+                uri: "https://kloud.benoit.fage.fr/api/photos/id/" + id,
+              };
+            });
+            const newData = [...data, ...fetched_data_].filter(onlyUnique);
+            setData(newData);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
     setRefreshing(false);
   };
-  const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
-    const color = item.id === selectedId ? "white" : "black";
-
-    return (
-      <Item
-        item={item}
-        onPress={() =>
-          navigation.navigate("ModalPhoto", {
-            id: item.id,
+  const onRefresh = async (start = -1) => {
+    setRefreshing(true);
+    SecureStore.getItemAsync("user_")
+      .then((user) => {
+        let userParsed = JSON.parse(user);
+        let headersList = {
+          Accept: "*/*",
+          "Content-Type": "application/json",
+          "auth-token": userParsed.token,
+        };
+        fetch(`https://kloud.benoit.fage.fr/api/photos/all/?start=0-&limit=${data.length + 2}`, {
+          method: "GET",
+          headers: headersList,
+        })
+          .then(function (response) {
+            return response.json();
           })
-        }
-        backgroundColor={{ backgroundColor }}
-        textColor={{ color }}
-      />
-    );
+          .then((fetched_data) => {
+            // TODO: Alert when fetched_data.data.length = 0;
+            console.log("fetched_onRefresh", fetched_data.data);
+            const fetched_data_ = fetched_data.data.map((id) => {
+              return {
+                id: id,
+                uri: "https://kloud.benoit.fage.fr/api/photos/id/" + id,
+              };
+            });
+            setData(fetched_data_);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    setRefreshing(false);
   };
   useEffect(() => {
-    
-  }, [])
+    onRefresh();
+  }, []);
+
+  useEffect(() => {
+    if (route.params?.refresh) {
+      setData([]);
+      onRefresh(0);
+      navigation.setParams({
+        refresh: false,
+      });
+    }
+  }, [route.params]);
+
+  const onPress = () => {
+    navigation.navigate("ModalUpload");
+  };
+
   return (
-    <SafeAreaView
-      style={tw`flex-1 items-center justify-between bg-[#F3F0E6] dark:bg-[#252525] pt-8`}
-    >
+    <SafeAreaView style={tw`flex-1 justify-between bg-[#F3F0E6] dark:bg-[#252525] pt-8`}>
       <StatusBar style="auto" />
       <FlatList
         ListHeaderComponent={() => {
           return (
             <View style={tw`flex-row justify-between w-full px-8 py-4`}>
-              <Text style={tw`text-4xl text-[#333333]`}>Photos</Text>
-              <Pressable onPress={handleLogOut}>
-                <SvgXml xml={logout} width={40} height={40} />
+              <Text style={tw`text-4xl text-[#666666]`}>Photos</Text>
+              <Pressable onPress={onPress}>
+                <SvgXml xml={add} width={40} height={40} />
               </Pressable>
             </View>
           );
@@ -149,15 +133,38 @@ export default function Photos({ navigation, logged, setLogged }) {
         style={tw`w-full`}
         numColumns={2}
         data={data}
-        renderItem={renderItem}
+        onEndReached={onEndReached}
+        renderItem={({ item, index }) => {
+          return (
+            <Pressable
+              onPress={() => {
+                navigation.navigate("ModalPhoto", {
+                  id: item.id,
+                });
+              }}
+              style={[tw`m-3 rounded-xl bg-[#ffffff]`, { width: vw(42), height: vw(42) }]}>
+              {/* TODO: Add progressive loading funct */}
+              <Image
+                source={{ uri: "https://kloud.benoit.fage.fr/api/photos/id/" + item.id }}
+                style={[
+                  tw`rounded-xl`,
+                  {
+                    width: vw(42),
+                    height: vw(42),
+                    resizeMode: "cover",
+                  },
+                ]}
+              />
+            </Pressable>
+          );
+        }}
         keyExtractor={(item) => item.id}
         extraData={selectedId}
-        contentContainerStyle={tw`items-center w-full`}
+        contentContainerStyle={[tw`items-start`, { marginLeft: vw(2) }]}
         ListHeaderComponentStyle={tw`w-full`}
-        refreshControl={
-          <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
-        }
+        refreshControl={<RefreshControl onRefresh={onRefresh} refreshing={refreshing} />}
       />
+      {data.length === 0 && <Text style={tw`text-[#777777]`}>No photos found.</Text>}
     </SafeAreaView>
   );
 }
