d2a05c591e1e398eb63d4fce935c9d05f6be5ed1
MrStaf <ben82160@gmail.com>
2021-11-13 18:41:50 +0100

    refactor: Pressable

--- src/front-end/components/buttonsLandPage.jsx
+++ src/front-end/components/buttonsLandPage.jsx
@@ -1,28 +1,26 @@
 import React from "react";
-import { Text, View, Image, TouchableHighlight } from "react-native";
+import { Text, View, Image, Pressable } from "react-native";
 import tw from "twrnc";
 
 export default function ButtonsLandPage({ navigation }) {
   return (
-    <TouchableHighlight
+    <Pressable
       onPress={() => navigation.navigate("SignIn")}
-      underlayColor="white"
     >
       <View
         style={tw`flex flex-row bg-[#ABCDCB] pr-10 items-center rounded-xl`}
       >
-        <TouchableHighlight
+        <Pressable
           onPress={() => navigation.navigate("Register")}
-          underlayColor="white"
         >
           <View
             style={tw`bg-[#60AEC2] py-4 px-10 mr-10 rounded-xl text-[#ffffff]`}
           >
             <Text style={tw`text-[#000]`}>Register</Text>
           </View>
-        </TouchableHighlight>
+        </Pressable>
         <Text style={tw`text-[#000]`}>Sign In</Text>
       </View>
-    </TouchableHighlight>
+    </Pressable>
   );
 }
