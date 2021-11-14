cc56e88945151de0d7534b0d06edf1c18effecb4
MrStaf <ben82160@gmail.com>
2021-11-14 23:06:57 +0100

    doc: Create documentation with APIDOCS

--- src/back-end/api/router.albums.js
+++ src/back-end/api/router.albums.js
@@ -6,8 +6,77 @@ const albumController = require("./../controllers/controller.albums");
 // Middleware
 const verify = require("../middleware/verifyToken");
 
-router.get("/", verify, albumController.getAlbum);
+/**
+ *
+ * @api {GET} /album/ Get all albums id
+ * @apiName getAlbums
+ * @apiGroup Albums
+ * @apiVersion  1.0.0
+ *
+ *
+ * @apiHeader {String} auth-token jwt token
+ *
+ * @apiSuccess (200) {String} status SUCCESS
+ * @apiSuccess (200) {String} message Album successfully got
+ * @apiSuccess (200) {Object[]} data array of Albums id
+ * 
+ * @apiSuccessExample {Object} Success-Response:
+ * {
+ *      "status": "SUCCESS",
+ *      "message": "Album successfully got",
+ *      "data": [
+ *          {
+ *          "_id": "617f236b68ca087c3865947b",
+ *          "userId": "617a6acc148003333520e04f",
+ *          "name": "new album 2",
+ *          "date": "2021-10-31T23:14:51.000Z",
+ *          "__v": 0
+ *          },
+ *          {
+ *          "_id": "617f236768ca087c38659479",
+ *          "userId": "617a6acc148003333520e04f",
+ *          "name": "new album",
+ *          "date": "2021-10-31T23:14:47.000Z",
+ *          "__v": 0
+ *          },
+ *          {
+ *          "_id": "617f2100ea07b57049ee5347",
+ *          "userId": "617a6acc148003333520e04f",
+ *          "date": "2021-10-31T23:04:32.000Z",
+ *          "__v": 0,
+ *          "name": "Test"
+ *          }
+ *      ]
+ * } 
+ *
+ *
+ */
+router.get("/", verify, albumController.getAlbums);
+router.get("/", verify, albumController.getMultipleAlbums);
 router.post("/", verify, albumController.addAlbum);
+
+/**
+ * 
+ * @api {DELETE} /album/id/:id Delete one album
+ * @apiName delAlbums
+ * @apiGroup Albums
+ * @apiVersion  1.0.0
+ * 
+ * @apiHeader {String} auth-token jwt token
+ * 
+ * @apiParam  {String} id album id
+ * 
+ * @apiSuccess (200) {String} status SUCCESS
+ * @apiSuccess (200) {String} message Album successfully deleted
+ * 
+ * @apiSuccessExample {Object} Success-Response:
+ * {
+ *      "status": "SUCCESS",
+ *      "message": "Album successfully deleted"
+ * }
+ * 
+ * 
+ */
 router.delete("/id/:id", verify, albumController.deleteAlbum);
 router.patch("/add", verify, albumController.addPhotoAlbum);
 router.patch("/remove", verify, albumController.removePhotoAlbum);
--- src/back-end/api/router.user.js
+++ src/back-end/api/router.user.js
@@ -7,7 +7,83 @@ const user = require("./../controllers/controller.users");
 // Middleware
 const verify = require("./../middleware/verifyToken");
 
+/**
+ * 
+ * @api {GET} /user/ Get user information
+ * @apiName getUser
+ * @apiGroup User
+ * @apiVersion  1.0.0
+ * 
+ * 
+ * @apiHeader  {String} auth-token jwt token
+ * 
+ * @apiSuccess {String} status SUCCESS
+ * @apiSuccess {String} message User successfully found
+ * @apiSuccess {String} data.firstName First Name 
+ * @apiSuccess {String} data.lastName Last Name
+ * @apiSuccess {String} data.email Email
+ * @apiSuccess {String} data.photoToken Token for later use (encryption)
+ * 
+ * @apiSuccessExample {Object} Success-Response:
+ *  {
+ *      "status": "SUCCESS",
+ *      "message": "User successfully found",
+ *      "data": {
+ *          "firstName": "MichMich",
+ *          "lastName": "Dupond",
+ *          "email": "some@email.com",
+ *          "photoToken": "8f12e418-30b1-40b2-8f12-c574fa723493"
+ *      }
+ *  }
+ * 
+ * 
+ */
 router.get("/", verify, user.getUser);
+/**
+ * 
+ * @api {POST} /user/signup Register a user
+ * @apiName registerUser
+ * @apiGroup User
+ * @apiVersion  1.0.0
+ * 
+ * @apiBody {String} firstName First Name
+ * @apiBody {String} lastName Last Name
+ * @apiBody {String} email Email
+ * @apiBody {String} password Password
+ * 
+ * @apiSuccess {String} status SUCCESS
+ * @apiSuccess {String} message Sign up successful
+ * @apiSuccess {String} token JWT token 
+ * @apiSuccess {String} data._id User id
+ * @apiSuccess {String} data.fistName Last Name
+ * @apiSuccess {String} data.lastName Last Name
+ * @apiSuccess {String} data.email Email
+ * @apiSuccess {String} data.password Hash of the password
+ * @apiSuccess {String} data.photoToken Token for later use (encryption)
+ * @apiSuccess {Number} data.__v Version of the document
+ * 
+ * 
+ * 
+ * @apiSuccessExample {type} Success-Response:
+ *  {
+ *      "status": "SUCCESS",
+ *      "message": "Sign up successful",
+ *      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpX9JVC.eyJfaWQiOiI2MTdhNmFjYzE0ODAwMzMzMzUyMGUwNGYiLCJpYXQiOjE2MzY5MjcxNjB9.9TXWZZGyK82UAfkFxqfuy5YNfv7G_cknUwPl0Qe1f5g",
+ *      "data": [
+ *          {
+ *          "_id": "617a6acc148003333520e04g",
+ *          "firstName": "MichMich",
+ *          "lastName": "Dupond",
+ *          "email": "some@email.com",
+ *          "password": "$2b$10$1255NmzK9ciPFHqcd8B6h.TK.ObsOiqsWIV0C5HPflhlB7xLyJqAG",
+ *          "photoToken": "8f12e418-30b1-40b2-8f12-c574fa723493",
+ *          "__v": 0
+ *          }
+ *      ]
+ *  }
+ * 
+ * 
+ */
 router.post("/signup", user.signUp);
 router.patch("/", verify, user.updateUser);
 router.post("/signin", user.signIn);
